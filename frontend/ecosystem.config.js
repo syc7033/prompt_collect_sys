732224ae78d83f7bdb358a78ef400db0d2ed8bb1
shiyichen03 <shiyichen03@kuaishou.com>
2026-04-17 12:41:57 +0800

    域名修改

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -8,14 +8,14 @@ module.exports = {
     interpreter: "/home/dev/.nvm/versions/node/v16.20.2/bin/node",
     env: {
       NODE_ENV: "development",
-      NEXT_PUBLIC_API_URL: "http://10.12.36.184:8000",
+      NEXT_PUBLIC_API_URL: "http://www.promptsystemsyc.asia:8000",
       NODE_PATH: "/home/dev/PromptSystem/frontend/node_modules",
       BABEL_ENV: "development",  // 添加Babel环境变量
       NEXT_BABEL_ENV: "development"
     },
     env_production: {
       NODE_ENV: "production",
-      NEXT_PUBLIC_API_URL: "http://10.12.36.184:8000",
+      NEXT_PUBLIC_API_URL: "http://www.promptsystemsyc.asia:8000",
       NODE_PATH: "/home/dev/PromptSystem/frontend/node_modules"
     }
   }]
